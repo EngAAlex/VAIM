7ffe6c16ee7bb90da508502b627298518685835f
Alessio Arleo <alessio.arleo@tuwien.ac.at>
2022-07-26 17:23:23 +0200

    Beautified install file + new demo dump

--- scripts/VAIM_install.sh
+++ scripts/VAIM_install.sh
@@ -10,45 +10,55 @@ echo Installing Prerequisites...
 sudo apt-get update -qq
 sudo apt-get install -y maven graphviz git-lfs npm openjdk-11-jdk
 
+echo
 echo DONE!
 echo 
-
+echo
 echo Installing Neo4J
-
+echo
 wget -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb https://debian.neo4j.com stable latest' | sudo tee -a /etc/apt/sources.list.d/neo4j.list
 sudo apt-get update -qy
 
 sudo apt-get install -y neo4j=1:4.4.8
-
+echo
 echo Installed Neo4J!
-
+echo
 ##### TO HERE. 
 
 echo Setting Neo4J default password and loading default database
 
 sudo neo4j-admin set-initial-password neo4j
-
+echo
 echo Retrieving files in LFS
-
+echo
 cd ..
 
 git lfs pull
 
+echo
 echo Building server
 
+echo
 cd server
 
 mvn clean package -DskipTests
 
+echo
 echo Loading Dump into Neo4j
+echo
 
 sudo neo4j-admin load --database=neo4j --force --from="dumps/livedemodump.dump"
 
+echo
 echo Done! Installing client
+echo
 
 cd ../client
 
 npm install
 
+echo
+echo
+echo
 echo All done! Start Neo4J and the client/server to get VAIM running.
